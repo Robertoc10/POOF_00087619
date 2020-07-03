b94bd6838d6ee9c2e54d39cc3c3692463cabca7a
Roberto <robertocabezas880@gmail.com>
2020-07-02 23:59:23 -0600

    Add: Manual

--- ParcialFinal/Vista/frmInitializeSesion.Designer.cs
+++ ParcialFinal/Vista/frmInitializeSesion.Designer.cs
@@ -39,6 +39,9 @@ namespace ParcialFinal.Vista
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(71, 57);
@@ -46,6 +49,9 @@ namespace ParcialFinal.Vista
             this.label1.Size = new System.Drawing.Size(162, 53);
             this.label1.TabIndex = 0;
             this.label1.Text = "Id de su Departamento:";
+            // 
+            // label2
+            // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label2.ForeColor = System.Drawing.Color.Blue;
             this.label2.Location = new System.Drawing.Point(71, 180);
@@ -53,6 +59,9 @@ namespace ParcialFinal.Vista
             this.label2.Size = new System.Drawing.Size(162, 53);
             this.label2.TabIndex = 1;
             this.label2.Text = "id de usuario:";
+            // 
+            // label3
+            // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label3.ForeColor = System.Drawing.Color.Blue;
             this.label3.Location = new System.Drawing.Point(71, 288);
@@ -60,15 +69,24 @@ namespace ParcialFinal.Vista
             this.label3.Size = new System.Drawing.Size(162, 53);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña:";
+            // 
+            // txtName
+            // 
             this.txtName.Location = new System.Drawing.Point(263, 78);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(326, 20);
             this.txtName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
             this.txtPassword.Location = new System.Drawing.Point(263, 293);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(326, 20);
             this.txtPassword.TabIndex = 5;
+            // 
+            // button1
+            // 
             this.button1.Location = new System.Drawing.Point(286, 348);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(208, 58);
@@ -76,11 +94,17 @@ namespace ParcialFinal.Vista
             this.button1.Text = "Iniciar sesion";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(263, 180);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(324, 21);
             this.comboBox1.TabIndex = 7;
+            // 
+            // frmInitializeSesion
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (128)))), ((int) (((byte) (255)))), ((int) (((byte) (128)))));
